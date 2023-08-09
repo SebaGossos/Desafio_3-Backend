8db46aaa1aae423ada5d6b0d04a524b30a3ef6b4
Sebastián Nicolás Gossos Scribe <111235284+WinniGossos33@users.noreply.github.com>
2023-08-09 10:26:10 -0300

    feat: read better product_manager

--- src/product_manager.js
+++ src/product_manager.js
@@ -19,6 +19,7 @@ export class ProductManager {
         }
         if( product ) products.push( product )
         await fs.promises.writeFile(this.#_path, JSON.stringify(products, null, 2) )
+    
         return products
     }
 
@@ -32,6 +33,7 @@ export class ProductManager {
         if ( !title || !description || !price || !thumbnail || !code || !stock ) throw new Error('Must submit all required fields')
         const products = await this.#prodJSON()
         if (products.some( p => p.code === code )) throw new Error(`Code: ${ code } must be unique, now is repetead!`);
+
         product.id = await this.generateId()
         await this.#prodJSON( product )
         return product
@@ -43,6 +45,7 @@ export class ProductManager {
         const products = await this.#prodJSON()
         const product = products.find( p => p.id === id )
         if ( !product ) throw new Error( `DIN´T FOUND A PRODUCT WITH ID: ${ id }`)
+
         return product
     }
     
@@ -52,8 +55,8 @@ export class ProductManager {
         let products = await this.#prodJSON()
         const isRepeteadCode = products.some( p => p.code === code )
         if ( isRepeteadCode ) throw new Error( `Code must be unique: ${ code }` )
-        const newProd = {}
 
+        const newProd = {}
         const newProducts = products.map( p => {
             if ( p.id === id ) {
                 for ( const prop in p ) newProd[prop] = product[prop] || p[prop];
@@ -61,7 +64,6 @@ export class ProductManager {
             }
             return p
         })
-
         await this.#prodJSON( null, newProducts )
         return newProducts
     }
@@ -69,6 +71,7 @@ export class ProductManager {
     async deleteProduct( id ) {
         let products = await this.getProducts()
         if ( !products.some( p => p.id === id )) throw new Error(`ID: ${ id } not found`);
+
         products = products.filter( p => p.id !== id )
         await this.#prodJSON( null, products )
         return products;
